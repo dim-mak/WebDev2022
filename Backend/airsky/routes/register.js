8553eea8299501ac7b2932332c66bec91a741075
eva.gaga <eva.gaga10@gmail.com>
2022-05-22 12:52:39 +0300

    passwrods checked for matching

--- Backend/airsky/routes/register.js
+++ Backend/airsky/routes/register.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const sqlite3 = require('sqlite3').verbose();
 const path = require("path");
 
@@ -19,15 +19,21 @@ router.get('/', function (req, res, next) {
 
 router.post('/add', function (req, res) {
     db.serialize(() => {
-        db.run('INSERT INTO USER(fname,lname,password,email,gender,street,street_no,city,region,zip_code,country) VALUES(?,?,?,?,?,?,?,?,?,?,?)',
-            [req.body.reg_first, req.body.reg_last, req.body.reg_pswd, req.body.reg_email, req.body.reg_sex, req.body.reg_str, req.body.reg_num,
-            req.body.reg_city, req.body.reg_state, req.body.reg_taxcode, req.body.reg_country], function (err) {
-                if (err) {
-                    return console.log(err.message);
-                }
-                console.log("New user added");
-                res.send("New user added to database with email = " + req.body.reg_email + " and name = " + req.body.reg_first + " " + req.body.reg_last);
-            });
+        if (req.body.reg_pswd != req.body.reg_pswdconf) {
+            console.log("Passwords do not match");
+            res.send("User not registered because passwords do not match");
+        }
+        else {
+            db.run('INSERT INTO USER(fname,lname,password,email,gender,street,street_no,city,region,zip_code,country) VALUES(?,?,?,?,?,?,?,?,?,?,?)',
+                [req.body.reg_first, req.body.reg_last, req.body.reg_pswd, req.body.reg_email, req.body.reg_sex, req.body.reg_str, req.body.reg_num,
+                req.body.reg_city, req.body.reg_state, req.body.reg_taxcode, req.body.reg_country], function (err) {
+                    if (err) {
+                        return console.log(err.message);
+                    }
+                    console.log("New user added");
+                    res.send("New user added to database with email = " + req.body.reg_email + " and name = " + req.body.reg_first + " " + req.body.reg_last);
+                });
+        }
     });
 });
 
